c53676cb2206bed46553b26ed3df23e4771b60e4
artursm14 <antonioartur123456@gmail.com>
2023-11-03 21:48:15 -0300

    integrando com js

---
+++ script.js
@@ -0,0 +1,20 @@
+function buscarCep(){
+    logradouro.value = "";
+    bairro.value = "";
+    cidade.value = "";
+    uf.value = "";
+
+
+    fetch(`http://viacep.com.br/ws/${cep.value}/json/`)
+    .then(function (resposta){
+        return resposta.json();
+    })
+    .then(function (conteudo){
+        logradouro.value = conteudo.logradouro;
+        bairro.value = conteudo.bairro;
+        cidade.value = conteudo.localidade;
+        uf.value = conteudo.uf;
+        
+    });
+}
+
